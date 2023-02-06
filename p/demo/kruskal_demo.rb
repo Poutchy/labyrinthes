43c91e86330779f970af23616cca10576a7a739a
Poutchy <enzo.mauti@laposte.net>
2023-02-06 14:02:02 +0100

    rematch des éléments python

--- p/demo/kruskal_demo.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../algorithms/kruskal'
-require_relative '../grid/gridColor'
-
-grid = Grid.new(20, 20)
-Kruskals.on(grid)
-
-filename = "images/kruskals.png"
-grid.to_png().save(filename)
-puts "saved to #{filename}"
