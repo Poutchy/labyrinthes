c5f9ba3ec503d69ac101b002362223cae60f2adf
Poutchy <enzo.mauti@laposte.net>
2023-02-23 15:31:49 +0100

    modification du test.rb

---
+++ test.rb
@@ -0,0 +1,18 @@
+require_relative 'lib/lib/ruby_vor'
+    require_relative 'lib/pp'
+
+    points = [
+      RubyVor::Point.new(120, 290),
+      RubyVor::Point.new(110, 120),
+      RubyVor::Point.new(160, 90.2),
+      RubyVor::Point.new(3.14159265, 3.14159265)
+    ]
+
+    # Compute the diagram & triangulation
+    comp = RubyVor::VDDT::Computation.from_points(points)
+
+    puts "The nearest-neighbor graph:"
+    pp comp.nn_graph
+
+    puts "\nThe minimum-spanning tree:"
+    pp comp.minimum_spanning_tree
