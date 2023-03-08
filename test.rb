bc90c4a6cc12ae87a0938e03983f623eacc7fae7
Poutchy <enzo.mauti@laposte.net>
2023-03-08 10:53:06 +0100

    modifications

--- test.rb
+++ test.rb
@@ -1,18 +1,44 @@
-require_relative 'lib/lib/ruby_vor'
-    require_relative 'lib/pp'
+require 'ruby_vor'
+require 'pp'
 
-    points = [
-      RubyVor::Point.new(120, 290),
-      RubyVor::Point.new(110, 120),
-      RubyVor::Point.new(160, 90.2),
-      RubyVor::Point.new(3.14159265, 3.14159265)
-    ]
+points = [
+  RubyVor::Point.new(120.0, 290.0),
+  RubyVor::Point.new(110.0, 120.0),
+  RubyVor::Point.new(200.0, 150.0),
+  RubyVor::Point.new(141.0, 219.0),
+  RubyVor::Point.new(180.0, 69.0),
+  RubyVor::Point.new(71.0, 75.0),
+  RubyVor::Point.new(183.0, 237.0),
+  RubyVor::Point.new(297.0, 121.0),
+  RubyVor::Point.new(67.0, 189.0),
+  RubyVor::Point.new(200.0, 200.0)
+]
+# visualizer svg
+# Compute the diagram & triangulation
+comp = RubyVor::VDDT::Computation.from_points(points)
 
-    # Compute the diagram & triangulation
-    comp = RubyVor::VDDT::Computation.from_points(points)
+puts "The nearest-neighbor graph:"
+pp comp.nn_graph
 
-    puts "The nearest-neighbor graph:"
-    pp comp.nn_graph
+puts "comp"
+puts comp.points
+puts "vDiagram"
+puts comp.voronoi_diagram_raw
+# puts "DenaulT"
+# puts comp.delaunay_triangulation_raw
+# puts "nnGraph"
+# puts comp.nn_graph
+# puts "mst"
+# puts comp.mst
+# puts "compu"
 
-    puts "\nThe minimum-spanning tree:"
-    pp comp.minimum_spanning_tree
+# RubyVor::Visualizer.make_svg(comp, :name => 'tri.svg')
+
+# Just the MST
+# RubyVor::Visualizer.make_svg(comp, :name => 'mst.svg', :triangulation => false, :mst => true)
+
+# Voronoi diagram and the triangulation
+# RubyVor::Visualizer.make_svg(comp, :name => 'dia.svg', :voronoi_diagram => true)
+
+# puts "\nThe minimum-spanning tree:"
+# pp comp.minimum_spanning_tree
